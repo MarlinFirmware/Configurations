6ec122c30c0bcebda1ae8eae67f6bda36a3e7f5a
Jason Smith <jason.inet@gmail.com>
2020-01-14 23:21:16 -0800

    Disable Tronxy XY100 PRINTCOUNTER, since there is no EEPROM in the configuration

--- config/examples/Tronxy/XY100/Configuration.h
+++ config/examples/Tronxy/XY100/Configuration.h
@@ -1579,7 +1579,7 @@
  *
  * View the current statistics with M78.
  */
-#define PRINTCOUNTER
+//#define PRINTCOUNTER
 
 //=============================================================================
 //============================= LCD and SD support ============================
