f3a6e52dcf822247de3a0cf6d5de0d1a9b376579
Scott Lahteine <github@thinkyhead.com>
2020-02-09 11:49:06 -0600

    Fix Geeetech machine names

--- config/examples/Geeetech/A20T/Configuration.h
+++ config/examples/Geeetech/A20T/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Geeetech A20M"
+#define CUSTOM_MACHINE_NAME "Geeetech A20T"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
