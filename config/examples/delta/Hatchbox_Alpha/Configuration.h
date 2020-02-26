d92808d3cfa589dbb1dc06adb5bd4929164039f7
Scott Lahteine <github@thinkyhead.com>
2020-02-26 13:06:50 -0600

    TRAVEL_EXTRA_XYJERK and GCODE_CASE_INSENSITIVE

--- config/examples/delta/Hatchbox_Alpha/Configuration.h
+++ config/examples/delta/Hatchbox_Alpha/Configuration.h
@@ -869,6 +869,8 @@
   #define DEFAULT_YJERK DEFAULT_XJERK
   #define DEFAULT_ZJERK DEFAULT_XJERK // Must be same as XY for delta
 
+  //#define TRAVEL_EXTRA_XYJERK 0.0     // Additional jerk allowance for all travel moves
+
   //#define LIMITED_JERK_EDITING        // Limit edit via M205 or LCD to DEFAULT_aJERK * 2
   #if ENABLED(LIMITED_JERK_EDITING)
     #define MAX_JERK_EDIT_VALUES { 20, 20, 0.6, 10 } // ...or, set your own edit limits
