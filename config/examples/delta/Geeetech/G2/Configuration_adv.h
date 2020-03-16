2ab97651efaeecada5c1a3c7bf3feca2e6db79a7
Scott Lahteine <github@thinkyhead.com>
2020-03-16 16:31:02 -0500

    Add SLOWDOWN_DIVISOR

--- config/examples/delta/Geeetech/G2/Configuration_adv.h
+++ config/examples/delta/Geeetech/G2/Configuration_adv.h
@@ -755,7 +755,12 @@
 
 // If defined the movements slow down when the look ahead buffer is only half full
 // (don't use SLOWDOWN with DELTA because DELTA generates hundreds of segments per second)
-//#define SLOWDOWN
+//// Slow down the machine if the look ahead buffer is (by default) half full.
+// Increase the slowdown divisor for larger buffer sizes.
+#define SLOWDOWN
+#if ENABLED(SLOWDOWN)
+  #define SLOWDOWN_DIVISOR 2
+#endif
 
 // Frequency limit
 // See nophead's blog for more info
