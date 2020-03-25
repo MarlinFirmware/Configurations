84c891e7aa162d364ba0095781960d736800213e
MohanMaker <31443287+MohanMaker@users.noreply.github.com>
2020-03-24 21:39:52 -0400

    Ender bed preheat temps (#34)

--- config/examples/Creality/Ender-5/Configuration.h
+++ config/examples/Creality/Ender-5/Configuration.h
@@ -1485,7 +1485,7 @@
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED      0
+#define PREHEAT_2_TEMP_BED     70
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
 /**
