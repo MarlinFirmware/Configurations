53b464902e2267ba41ba838d2c3e1f3a0422d9ca
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-04-12 15:29:57 -0700

    Alternative Wanhao Duplicator i3 Mini bootscreens (#57)

--- config/examples/Wanhao/Duplicator i3 Mini/_Bootscreen.h
+++ config/examples/Wanhao/Duplicator i3 Mini/_Bootscreen.h
@@ -1,9 +1,32 @@
 /**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#pragma once
+
+/**
  * Made with Marlin Bitmap Converter
  * https://marlinfw.org/tools/u8glib/converter.html
  */
+
 #define CUSTOM_BOOTSCREEN_BMPWIDTH  80
-#define CUSTOM_BOOTSCREEN_Y          3
 
 const unsigned char custom_start_bmp[] PROGMEM = {
   B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000111,B11000000,B00000000,B00000000,
