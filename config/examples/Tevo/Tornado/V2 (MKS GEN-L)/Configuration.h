136bf802645cdbd10af20d39bd8a6586ad322fb1
andi448 <37498627+andi448@users.noreply.github.com>
2020-05-06 06:37:49 +0200

    Update Configuration.h

--- config/examples/Tevo/Tornado/V2 (MKS GEN-L)/Configuration.h
+++ config/examples/Tevo/Tornado/V2 (MKS GEN-L)/Configuration.h
@@ -1107,7 +1107,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 320
+#define X_BED_SIZE 310
 #define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
