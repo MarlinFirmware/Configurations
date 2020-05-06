20d113bb131bb5cdc4513c9ff7ebcd023ccbbd87
andi448 <37498627+andi448@users.noreply.github.com>
2020-05-06 08:07:13 +0200

    Fix Tornado bed width (#88)

--- config/examples/Tevo/Tornado/V2 (MKS GEN-L)/Configuration.h
+++ config/examples/Tevo/Tornado/V2 (MKS GEN-L)/Configuration.h
@@ -1109,7 +1109,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 320
+#define X_BED_SIZE 310
 #define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
