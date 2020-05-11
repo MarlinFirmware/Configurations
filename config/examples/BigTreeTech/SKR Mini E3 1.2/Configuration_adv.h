4036805b9f6239302a3ddc7c842f22ec7bb7cf37
thisiskeithb <thisiskeithb@outlook.com>
2020-05-10 17:16:54 -0700

    Increase Z Hybrid Threshold in SKR Mini E3 configs

--- config/examples/BigTreeTech/SKR Mini E3 1.2/Configuration_adv.h
+++ config/examples/BigTreeTech/SKR Mini E3 1.2/Configuration_adv.h
@@ -2348,7 +2348,7 @@
   #define X2_HYBRID_THRESHOLD    100
   #define Y_HYBRID_THRESHOLD     100
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
+  #define Z_HYBRID_THRESHOLD      30
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
