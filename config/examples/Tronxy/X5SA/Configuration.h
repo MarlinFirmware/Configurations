3c617340e318a12d6464a7c3320685e6e2a67188
Victor Mateus Oliveira <rhapsodyv@gmail.com>
2020-06-10 10:25:27 -0300

    fixed offsets for a more conservative with corners

--- config/examples/Tronxy/X5SA/Configuration.h
+++ config/examples/Tronxy/X5SA/Configuration.h
@@ -974,11 +974,11 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 30
+#define PROBING_MARGIN 50
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
