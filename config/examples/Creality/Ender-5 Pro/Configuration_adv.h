616b43711589fcdbbbcd3d4d282af0d20d4fd0cc
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-06-21 19:21:14 -0500

    Update Configuration_adv.h

--- config/examples/Creality/Ender-5 Pro/Configuration_adv.h
+++ config/examples/Creality/Ender-5 Pro/Configuration_adv.h
@@ -629,6 +629,7 @@
  */
 
 //#define SENSORLESS_BACKOFF_MM  { 2, 2 }     // (mm) Backoff from endstops before sensorless homing
+
 #define HOMING_BUMP_MM      { 5, 5, 2 }       // (mm) Backoff from endstops after first bump
 #define HOMING_BUMP_DIVISOR { 2, 2, 4 }       // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 
