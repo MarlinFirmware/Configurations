0c80b0509a32550a7fe7471dec5716184602f614
Raoul Rubien <rubienr@sbox.tugraz.at>
2020-06-28 13:13:12 +0200

    modified wrong feed rates, assumed mm/minute (wrong) instead mm/second (correct)

--- config/examples/Creality/Ender-5_Plus/Configuration.h
+++ config/examples/Creality/Ender-5_Plus/Configuration.h
@@ -744,7 +744,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 8000, 8000, 15, 15000 }
+#define DEFAULT_MAX_FEEDRATE          { 800, 800, 15, 1500 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
