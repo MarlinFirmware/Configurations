8fcc9faf2d831fbb7a4129cad1b800cca6196727
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-06-28 23:38:29 -0500

    Update Configuration.h

--- config/examples/delta/Geeetech/G2S/Configuration.h
+++ config/examples/delta/Geeetech/G2S/Configuration.h
@@ -1187,8 +1187,7 @@
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
 #define INVERT_E0_DIR true
-// Geeetech G2S usually comes with a "mirrored" feeder, so the secondary extrudes has to run in the opposite direction
-#define INVERT_E1_DIR false
+#define INVERT_E1_DIR false  // Geeetech G2S usually comes with a "mirrored" feeder running in the opposite direction
 #define INVERT_E2_DIR true
 #define INVERT_E3_DIR true
 #define INVERT_E4_DIR true
