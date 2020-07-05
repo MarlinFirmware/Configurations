784aab1709b9ba555bfa7ecac09b888b47b9d795
Scott Lahteine <github@thinkyhead.com>
2020-07-04 21:41:24 -0500

    TFT_LITTLE_VGL_UI => TFT_LVGL_UI

--- config/examples/RepRapPro/Huxley/Configuration.h
+++ config/examples/RepRapPro/Huxley/Configuration.h
@@ -2190,9 +2190,12 @@ Black rubber belt(MXL), 18 - tooth aluminium pulley : 87.489 step per mm (Huxley
 //#define FSMC_GRAPHICAL_TFT
 
 //
-// TFT Little VGL UI
+// TFT LVGL UI
 //
-//#define TFT_LITTLE_VGL_UI
+// Default MKS icons and fonts: https://git.io/JJvzK
+// Copy mks_pic and mks_font folders to the root of your SD
+//
+//#define TFT_LVGL_UI
 
 //=============================================================================
 //============================  Other Controllers  ============================
