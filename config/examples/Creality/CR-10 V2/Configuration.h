738fa0b605c634bef118b50f720de943097eef30
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-07-05 18:07:27 -0500

    for fun…

--- config/examples/Creality/CR-10 V2/Configuration.h
+++ config/examples/Creality/CR-10 V2/Configuration.h
@@ -679,15 +679,9 @@
 #define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#if ENABLED(BLTOUCH)
-  #define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-  #define Z_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-  #define Z_MIN_PROBE_ENDSTOP_INVERTING true // Set to true to invert the logic of the probe.
-#else
-  #define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-  #define Z_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-  #define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe.
-#endif
+#define Z_MIN_ENDSTOP_INVERTING ENABLED(BLTOUCH) // Set to true to invert the logic of the endstop.
+#define Z_MAX_ENDSTOP_INVERTING ENABLED(BLTOUCH) // Set to true to invert the logic of the endstop.
+#define Z_MIN_PROBE_ENDSTOP_INVERTING ENABLED(BLTOUCH) // Set to true to invert the logic of the probe.
 
 /**
  * Stepper Drivers
