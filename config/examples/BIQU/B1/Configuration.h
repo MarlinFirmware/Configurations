87ed60d3aa8a4b58c0f08d0ebd0869112b4eb4de
thisiskeithb <thisiskeithb@outlook.com>
2020-08-15 07:09:42 -0700

    Only two neopixels

--- config/examples/BIQU/B1/Configuration.h
+++ config/examples/BIQU/B1/Configuration.h
@@ -2371,7 +2371,7 @@
   //#define NEOPIXEL_PIN     4     // LED driving pin
   //#define NEOPIXEL2_TYPE NEOPIXEL_TYPE
   //#define NEOPIXEL2_PIN    5
-  #define NEOPIXEL_PIXELS 4        // Number of LEDs in the strip, larger of 2 strips if 2 neopixel strips are used
+  #define NEOPIXEL_PIXELS 2        // Number of LEDs in the strip, larger of 2 strips if 2 neopixel strips are used
   //#define NEOPIXEL_IS_SEQUENTIAL // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
   #define NEOPIXEL_BRIGHTNESS 127  // Initial brightness (0-255)
   #define NEOPIXEL_STARTUP_TEST    // Cycle through colors at startup
