d48d76739d970dd311dba4b65eb451e2d92b2a2b
Maximilian Weiler <16721506+maweil@users.noreply.github.com>
2020-08-20 02:32:52 +0200

    Add SDCARD_READONLY to Ender-3 (and Pro) (#178)

--- config/examples/Creality/Ender-3 Pro/Configuration_adv.h
+++ config/examples/Creality/Ender-3 Pro/Configuration_adv.h
@@ -1124,7 +1124,9 @@
   // Enable this option and set to HIGH if your SD cards are incorrectly detected.
   //#define SD_DETECT_STATE HIGH
 
-  //#define SDCARD_READONLY                 // Read-only SD card (to save over 2K of flash)
+  #if NONE(POWER_LOSS_RECOVERY, BINARY_FILE_TRANSFER, SDCARD_EEPROM_EMULATION)
+    #define SDCARD_READONLY                 // Read-only SD card (to save over 2K of flash)
+  #endif
 
   #define SD_PROCEDURE_DEPTH 1              // Increase if you need more nested M32 calls
 
