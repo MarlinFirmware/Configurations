b74d561ab87be806844b2e6b02a2e8e983857258
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-08-21 21:43:41 -0500

    ACTIVE_HIGH=>ACTIVE_STATE

--- config/examples/Modix/Big60/Configuration.h
+++ config/examples/Modix/Big60/Configuration.h
@@ -332,9 +332,9 @@
 //#define PSU_NAME "Power Supply"
 
 #if ENABLED(PSU_CONTROL)
-  #define PSU_ACTIVE_HIGH false     // Set 'false' for ATX (1), 'true' for X-Box (2)
+  #define PSU_ACTIVE_STATE LOW      // Set 'LOW' for ATX, 'HIGH' for X-Box
 
-  //#define PS_DEFAULT_OFF          // Keep power off until enabled directly with M80
+  //#define PSU_DEFAULT_OFF         // Keep power off until enabled directly with M80
   //#define PSU_POWERUP_DELAY 250   // (ms) Delay for the PSU to warm up to full power
 
   //#define AUTO_POWER_CONTROL      // Enable automatic control of the PS_ON pin
--- config/examples/delta/kossel_xl/Configuration.h
+++ config/examples/delta/kossel_xl/Configuration.h
@@ -332,7 +332,7 @@
 //#define PSU_NAME "Power Supply"
 
 #if ENABLED(PSU_CONTROL)
-  #define PSU_ACTIVE_HIGH true      // Set 'false' for ATX, 'true' for X-Box
+  #define PSU_ACTIVE_STATE HIGH     // Set 'LOW' for ATX, 'HIGH' for X-Box
 
   //#define PSU_DEFAULT_OFF         // Keep power off until enabled directly with M80
   //#define PSU_POWERUP_DELAY 250   // (ms) Delay for the PSU to warm up to full power
