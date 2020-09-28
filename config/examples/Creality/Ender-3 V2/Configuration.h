b5b5d00406e3e45810ddd41ff88e052b194d4ea8
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-09-28 17:24:43 -0500

    Ender 3 V2 PID label

--- config/examples/Creality/Ender-3 V2/Configuration.h
+++ config/examples/Creality/Ender-3 V2/Configuration.h
@@ -533,10 +533,9 @@
   //#define MIN_BED_POWER 0
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
-  // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-  // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
+  // Ender 3 V2
   #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi 85.47
+  #define DEFAULT_bedKi  85.47
   #define DEFAULT_bedKd 624.59
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
