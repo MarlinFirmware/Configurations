51efea51915f05e8855d044ae3408bc5d3e2bc10
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-09-28 18:34:53 -0500

    No POWER_LOSS_ZHOME_POS

--- config/examples/HMS434/Configuration_adv.h
+++ config/examples/HMS434/Configuration_adv.h
@@ -1159,17 +1159,13 @@
   #if ENABLED(POWER_LOSS_RECOVERY)
     #define PLR_ENABLED_DEFAULT   false // Power Loss Recovery enabled by default. (Set with 'M413 Sn' & M500)
     //#define BACKUP_POWER_SUPPLY       // Backup power / UPS to move the steppers on power loss
+    //#define POWER_LOSS_RECOVER_ZHOME  // Z homing is needed for proper recovery. 99.9% of the time this should be disabled!
     //#define POWER_LOSS_ZRAISE       2 // (mm) Z axis raise on resume (on power loss with UPS)
     //#define POWER_LOSS_PIN         44 // Pin to detect power loss. Set to -1 to disable default pin on boards without module.
     //#define POWER_LOSS_STATE     HIGH // State of pin indicating power loss
     //#define POWER_LOSS_PULL           // Set pullup / pulldown as appropriate
     //#define POWER_LOSS_PURGE_LEN   20 // (mm) Length of filament to purge on resume
     //#define POWER_LOSS_RETRACT_LEN 10 // (mm) Length of filament to retract on fail. Requires backup power.
-
-    //#define POWER_LOSS_RECOVER_ZHOME  // Z homing is needed for proper recovery. 99.9% of the time this should be disabled!
-    #if ENABLED(POWER_LOSS_RECOVER_ZHOME)
-      #define POWER_LOSS_ZHOME_POS { (X_BED_SIZE) / 2, 0 }  // (mm) Safe XY position for homing Z to avoid print
-    #endif
 
     // Without a POWER_LOSS_PIN the following option helps reduce wear on the SD card,
     // especially with "vase mode" printing. Set too high and vases cannot be continued.
