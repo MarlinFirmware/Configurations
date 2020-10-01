a34a359be57632a6a706db2264cbea01b8a416de
Andreas Setterlind <Gamester17@users.noreply.github.com>
2020-10-01 19:51:58 +0200

    Define CUSTOM_MACHINE_NAME as "CR-10S"
    Set #define CUSTOM_MACHINE_NAME "CR-10S"

--- config/examples/Creality/CR-10S/BigTreeTech SKR Mini E3 2.0/Configuration.h
+++ config/examples/Creality/CR-10S/BigTreeTech SKR Mini E3 2.0/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "CR-10"
+#define CUSTOM_MACHINE_NAME "CR-10S"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
