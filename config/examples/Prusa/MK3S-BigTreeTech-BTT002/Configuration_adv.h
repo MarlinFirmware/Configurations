46dce0d59465cf2e07b3d072b0f6ef0a3e8455af
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-10-14 13:47:27 -0700

    Disable hybrid threshold

--- config/examples/Prusa/MK3S-BigTreeTech-BTT002/Configuration_adv.h
+++ config/examples/Prusa/MK3S-BigTreeTech-BTT002/Configuration_adv.h
@@ -2503,13 +2503,13 @@
    * STEALTHCHOP_(XY|Z|E) must be enabled to use HYBRID_THRESHOLD.
    * M913 X/Y/Z/E to live tune the setting
    */
-  #define HYBRID_THRESHOLD
+  //#define HYBRID_THRESHOLD
 
-  #define X_HYBRID_THRESHOLD     120  // [mm/s]
+  #define X_HYBRID_THRESHOLD     100  // [mm/s]
   #define X2_HYBRID_THRESHOLD    100
-  #define Y_HYBRID_THRESHOLD     120
+  #define Y_HYBRID_THRESHOLD     100
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD     100
+  #define Z_HYBRID_THRESHOLD       3
   #define Z2_HYBRID_THRESHOLD      3
   #define Z3_HYBRID_THRESHOLD      3
   #define Z4_HYBRID_THRESHOLD      3
