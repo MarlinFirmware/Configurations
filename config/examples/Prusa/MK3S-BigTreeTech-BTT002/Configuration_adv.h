45d2cc7c724f1d433fef0ddef10864e6f8459450
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-10-14 13:43:37 -0700

    Enable MECHANICAL_GANTRY_CALIBRATION

--- config/examples/Prusa/MK3S-BigTreeTech-BTT002/Configuration_adv.h
+++ config/examples/Prusa/MK3S-BigTreeTech-BTT002/Configuration_adv.h
@@ -3381,9 +3381,9 @@
  * Adds capability to work with any adjustable current drivers.
  * Implemented as G34 because M915 is deprecated.
  */
-//#define MECHANICAL_GANTRY_CALIBRATION
+#define MECHANICAL_GANTRY_CALIBRATION
 #if ENABLED(MECHANICAL_GANTRY_CALIBRATION)
-  #define GANTRY_CALIBRATION_CURRENT          600     // Default calibration current in ma
+  #define GANTRY_CALIBRATION_CURRENT    (Z_CURRENT/2) // Default calibration current in ma
   #define GANTRY_CALIBRATION_EXTRA_HEIGHT      15     // Extra distance in mm past Z_###_POS to move
   #define GANTRY_CALIBRATION_FEEDRATE         500     // Feedrate for correction move
   //#define GANTRY_CALIBRATION_TO_MIN                 // Enable to calibrate Z in the MIN direction
