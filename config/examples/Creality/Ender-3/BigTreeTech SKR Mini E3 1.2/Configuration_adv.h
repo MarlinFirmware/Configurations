9304c3e33b0645e8b8b70bd505ae6f197871ad41
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2020-10-14 14:10:34 -0700

    Restore Ender-3 / SKR Mini E3 1.2 current (#267)

--- config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 1.2/Configuration_adv.h
+++ config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 1.2/Configuration_adv.h
@@ -2329,7 +2329,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      770
+    #define E0_CURRENT      650
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
