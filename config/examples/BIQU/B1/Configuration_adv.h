57c578370f4956b317eb85b0db1c311a342ac3c7
Scott Lahteine <github@thinkyhead.com>
2020-10-14 23:36:59 -0500

    Config version 020008

--- config/examples/BIQU/B1/Configuration_adv.h
+++ config/examples/BIQU/B1/Configuration_adv.h
@@ -30,7 +30,7 @@
  *
  * Basic settings can be found in Configuration.h
  */
-#define CONFIGURATION_ADV_H_VERSION 020007
+#define CONFIGURATION_ADV_H_VERSION 020008
 
 //===========================================================================
 //============================= Thermal Settings ============================
--- config/examples/Creality/CR-10 Mini/CrealityV1/Configuration.h
+++ config/examples/Creality/CR-10 Mini/CrealityV1/Configuration.h
@@ -44,7 +44,7 @@
  * CR10_STOCKDISPLAY (RAMPS-compatible with single 10-pin plug)
  */
 
-#define CONFIGURATION_H_VERSION 020007
+#define CONFIGURATION_H_VERSION 020008
 
 //===========================================================================
 //============================= Getting Started =============================
