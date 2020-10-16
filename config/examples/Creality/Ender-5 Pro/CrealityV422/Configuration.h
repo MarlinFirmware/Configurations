c142cdfa09d2096b11ae4d4eefc8129f988b24a7
Kyle Gordon <kyle@lodge.glasgownet.com>
2020-10-16 12:08:09 +0100

    New machine name

--- config/examples/Creality/Ender-5 Pro/CrealityV422/Configuration.h
+++ config/examples/Creality/Ender-5 Pro/CrealityV422/Configuration.h
@@ -131,7 +131,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-5 Pro 4.2.7"
+#define CUSTOM_MACHINE_NAME "Ender-5 Pro 4.2.2"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
