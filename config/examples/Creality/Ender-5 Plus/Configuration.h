b6b838bdd59832a3ba068172a63877ccd0bd3ca4
Scott Lahteine <thinkyhead@users.noreply.github.com>
2020-10-29 20:22:03 -0500

    Tweak

--- config/examples/Creality/Ender-5 Plus/Configuration.h
+++ config/examples/Creality/Ender-5 Plus/Configuration.h
@@ -2172,11 +2172,14 @@
 
 //
 // DGUS Touch Display with DWIN OS. (Choose one.)
+//
 // Flash display with DGUS Displays for Marlin:
-// 1.) Format the SD card to FAT32 with a allocation size of 4k
-// 2.) Copy DWIN_SET folder from [1] to SD card,
-// 3.) boot the display with SD plugged into its own SD reader (not the motherboard's).
-// [1] https://github.com/coldtobi/Marlin_DGUS_Resources
+//  1. Format the SD card to FAT32 with a allocation size of 4k.
+//  2. Download https://github.com/coldtobi/Marlin_DGUS_Resources
+//  3. Copy the downloaded DWIN_SET folder to the SD card.
+//  4. Plug the microSD card into the back of the display.
+//  4. Boot the display and wait for the update to complete.
+//
 #define DGUS_LCD_UI_ORIGIN
 //#define DGUS_LCD_UI_FYSETC
 //#define DGUS_LCD_UI_HIPRECY
