27d014a7a624bd29381526322a8349110f829383
Scott Lahteine <github@thinkyhead.com>
2020-11-11 00:34:45 -0600

    MMU2 eject / load / ramming

--- config/default/Configuration_adv.h
+++ config/default/Configuration_adv.h
@@ -3543,7 +3543,7 @@
   // Add an LCD menu for MMU2
   //#define MMU2_MENUS
 
-  // Settings for filament load / unload.
+  // Settings for filament load / unload from the LCD menu.
   // This is for Průša MK3-style extruders. Customize for your hardware.
   #define MMU2_FILAMENTCHANGE_EJECT_FEED 80.0
   #define MMU2_LOAD_TO_NOZZLE_SEQUENCE \
