14567210c7aede370a8ab110573777dba15dd6a8
Axel <ansepulveda@uc.cl>
2020-11-11 01:07:47 -0300

    it is E16 not A8+

--- config/examples/Anet/E16/BTT SKR 1.3/Configuration.h
+++ config/examples/Anet/E16/BTT SKR 1.3/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(chepo92, A8+ SKR)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(chepo92, E16 SKR)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Anet A8+ SKR"
+#define CUSTOM_MACHINE_NAME "E16+ SKR"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
