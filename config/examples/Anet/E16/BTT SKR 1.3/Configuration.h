adfef10734e1bfea5fa4e5bf7174de4407c515ff
Axel <ansepulveda@uc.cl>
2020-11-12 00:47:31 -0300

    update steps per mm for stock hw

--- config/examples/Anet/E16/BTT SKR 1.3/Configuration.h
+++ config/examples/Anet/E16/BTT SKR 1.3/Configuration.h
@@ -132,7 +132,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "E16+ SKR"
+#define CUSTOM_MACHINE_NAME "E16 SKR"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -748,7 +748,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 415 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
