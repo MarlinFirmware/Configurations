fd2a70d0d504a92d68cbb263f9639ae72962853b
“ils15” <igorleite-ils@hotmail.com>
2020-11-18 12:42:07 -0300

    fix TMC2209 Steps for BlueR V3

--- config/examples/Two Trees/BlueR/BlueR V3/Configuration.h
+++ config/examples/Two Trees/BlueR/BlueR V3/Configuration.h
@@ -747,7 +747,11 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 400, 415 }
+ 
+#ifdef BLUER_TMC2209
+ #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 415 }
+#else
+ #define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 400, 415 } 
 
 /**
  * Default Max Feed Rate (mm/s)
