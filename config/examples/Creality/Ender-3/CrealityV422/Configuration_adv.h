55bc35eb7f882725e99e9f709c50ab664df8fea1
Jimmy Wan <jimmywan@users.noreply.github.com>
2020-12-13 20:31:06 -0600

    Fix PROBE_OFFSET_WIZARD_START_Z (#353)

--- config/examples/Creality/Ender-3/CrealityV422/Configuration_adv.h
+++ config/examples/Creality/Ender-3/CrealityV422/Configuration_adv.h
@@ -1084,7 +1084,7 @@
   #if HAS_BED_PROBE
     //#define PROBE_OFFSET_WIZARD
     #if ENABLED(PROBE_OFFSET_WIZARD)
-      #define PROBE_OFFSET_START -4.0   // Estimated nozzle-to-probe Z offset, plus a little extra
+      #define PROBE_OFFSET_WIZARD_START_Z -4.0   // Estimated nozzle-to-probe Z offset, plus a little extra
       //#define PROBE_OFFSET_WIZARD_XY_POS XY_CENTER // Set a convenient position to do the measurement
     #endif
   #endif
