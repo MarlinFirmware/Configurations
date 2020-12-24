d6c8c4c303f37e1d183bf9b179a8a9ca72c025c4
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-12-24 06:46:51 -0800

    Remove old robin TFT32 defines

--- config/examples/Anet/ET4-Pro/Configuration.h
+++ config/examples/Anet/ET4-Pro/Configuration.h
@@ -2345,7 +2345,7 @@
 // 320x240, 3.2", FSMC Display From MKS
 // Normally used in MKS Robin Nano V1.2
 //
-#define MKS_ROBIN_TFT32
+//#define MKS_ROBIN_TFT32
 
 //
 // 480x320, 3.5", FSMC Display From MKS
