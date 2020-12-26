532d0f8a46d40efc2a6cf39b71f38a8fd659ef56
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2020-12-25 23:28:23 -0800

    Fix ET4 X EXTRUDE_MAXLENGTH
    Needs to be >= FILAMENT_CHANGE_UNLOAD_LENGTH

--- config/examples/Anet/ET4X/Configuration.h
+++ config/examples/Anet/ET4X/Configuration.h
@@ -565,7 +565,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 100
+#define EXTRUDE_MAXLENGTH 350
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
