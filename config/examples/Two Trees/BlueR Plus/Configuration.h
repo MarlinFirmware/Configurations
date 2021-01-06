6f3a37cb5cdb3c46d6795448b24e3ba5345e61de
ils15 <igorleite-ils@hotmail.com>
2021-01-06 18:58:53 -0300

    update PID parameters/ Enable Power LOSS by defaut

--- config/examples/Two Trees/BlueR Plus/Configuration.h
+++ config/examples/Two Trees/BlueR Plus/Configuration.h
@@ -494,11 +494,14 @@
     #define DEFAULT_Kd_LIST { 48.43, 48.43 }
   #else
     // Bluer
-    #define DEFAULT_Kp 11.03
-    #define DEFAULT_Ki  0.63
-    #define DEFAULT_Kd 48.43
+    #define DEFAULT_Kp 24.6
+    #define DEFAULT_Ki 2.90
+    #define DEFAULT_Kd 52.3
   #endif
 #endif // PIDTEMP
+
+
+
 
 //===========================================================================
 //====================== PID > Bed Temperature Control ======================
@@ -535,9 +538,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp  60.1
+  #define DEFAULT_bedKi  8.07
+  #define DEFAULT_bedKd 298.9
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
@@ -2453,6 +2456,7 @@
 	#define TOUCH_CALIBRATION_X 16128
 	#define TOUCH_CALIBRATION_Y 10841
 	#define TOUCH_OFFSET_X -13
+	#define TOUCH_OFFSET_Y -36
 	#define TOUCH_OFFSET_Y -36
   //#define TOUCH_ORIENTATION TOUCH_LANDSCAPE
   
