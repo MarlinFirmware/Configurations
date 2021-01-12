a759a4744ba7c81fd2ca9ff2eee16dbca3493b0d
ils15 <igorleite-ils@hotmail.com>
2021-01-12 13:36:07 -0300

    Active Level Bed by default

--- config/examples/Two Trees/Sapphire Pro/Configuration.h
+++ config/examples/Two Trees/Sapphire Pro/Configuration.h
@@ -1476,7 +1476,7 @@
 #endif
 
 // Add a menu item to move between bed corners for manual bed adjustment
-//#define LEVEL_BED_CORNERS
+#define LEVEL_BED_CORNERS
 
 #if ENABLED(LEVEL_BED_CORNERS)
   #define LEVEL_CORNERS_INSET_LFRB { 30, 30, 30, 30 } // (mm) Left, Front, Right, Back insets
