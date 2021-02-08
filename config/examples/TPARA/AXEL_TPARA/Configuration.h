531e200076f6d655e6aeafa609b1494e7b49eb2e
Axel <ansepulveda@uc.cl>
2021-02-08 15:46:26 -0300

    upd home pos

--- config/examples/TPARA/AXEL_TPARA/Configuration.h
+++ config/examples/TPARA/AXEL_TPARA/Configuration.h
@@ -1577,8 +1577,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS   0
-#define MANUAL_Y_HOME_POS  35
+#define MANUAL_X_HOME_POS  35
+#define MANUAL_Y_HOME_POS   0
 #define MANUAL_Z_HOME_POS 125
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
