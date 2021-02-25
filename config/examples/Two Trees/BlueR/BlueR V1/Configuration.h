f012a3355439f5979408318b7085c322d2638c40
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-02-25 01:58:06 -0600

    Update Configuration.h

--- config/examples/Two Trees/BlueR/BlueR V1/Configuration.h
+++ config/examples/Two Trees/BlueR/BlueR V1/Configuration.h
@@ -501,7 +501,7 @@
   #else
     // Bluer
     #define DEFAULT_Kp 15.72
-    #define DEFAULT_Ki 1.24
+    #define DEFAULT_Ki  1.24
     #define DEFAULT_Kd 49.77
   #endif
 #endif // PIDTEMP
@@ -542,8 +542,8 @@
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
   //BlueR PID Settings
-  #define DEFAULT_bedKp 66.39
-  #define DEFAULT_bedKi 12.94
+  #define DEFAULT_bedKp  66.39
+  #define DEFAULT_bedKi  12.94
   #define DEFAULT_bedKd 227.14
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
