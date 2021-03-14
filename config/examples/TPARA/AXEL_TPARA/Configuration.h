3cc9fc5cddcab6226f2562736ac830f2f1e3ed31
StuSerious <stefano.serioli@outlook.com>
2021-03-14 11:45:35 +0100

    semi-auto replace, more fixes

--- config/examples/TPARA/AXEL_TPARA/Configuration.h
+++ config/examples/TPARA/AXEL_TPARA/Configuration.h
@@ -458,8 +458,8 @@
 #define MAX_REDUNDANT_TEMP_SENSOR_DIFF 10
 
 #define TEMP_RESIDENCY_TIME      3  // (seconds) Time to wait for hotend to "settle" in M109
-#define TEMP_WINDOW              1  // (°C) Temperature proximity for the "temperature reached" timer
-#define TEMP_HYSTERESIS          3  // (°C) Temperature proximity considered "close enough" to the target
+#define TEMP_WINDOW                  1  // (°C) Temperature proximity for the "temperature reached" timer
+#define TEMP_HYSTERESIS              3  // (°C) Temperature proximity considered "close enough" to the target
 
 #define TEMP_BED_RESIDENCY_TIME  0  // (seconds) Time to wait for bed to "settle" in M190
 #define TEMP_BED_WINDOW          1  // (°C) Temperature proximity for the "temperature reached" timer
