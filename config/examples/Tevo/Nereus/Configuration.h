39c37c02ea966c218c90d046d026d03cf0c78347
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2021-03-25 15:30:45 -0700

    Assume nominal belts/pulleys

--- config/examples/Tevo/Nereus/Configuration.h
+++ config/examples/Tevo/Nereus/Configuration.h
@@ -799,7 +799,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.4, 80.4, 400, 370 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 370 }
 
 /**
  * Default Max Feed Rate (mm/s)
