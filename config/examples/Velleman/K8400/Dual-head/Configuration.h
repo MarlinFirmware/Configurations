22e1a75f3ad8236000902ac99edcf768041d1175
Scott Lahteine <github@thinkyhead.com>
2021-03-29 00:44:16 -0500

    more

--- config/examples/Velleman/K8400/Dual-head/Configuration.h
+++ config/examples/Velleman/K8400/Dual-head/Configuration.h
@@ -437,9 +437,9 @@
 //#define TEMP_SENSOR_1_AS_REDUNDANT
 #define MAX_REDUNDANT_TEMP_SENSOR_DIFF 10
 
-#define TEMP_RESIDENCY_TIME 2   // (seconds) Time to wait for hotend to "settle" in M109
-#define TEMP_HYSTERESIS 5       // (°C) Temperature proximity considered "close enough" to the target
-#define TEMP_WINDOW     1       // (°C) Temperature proximity for the "temperature reached" timer
+#define TEMP_RESIDENCY_TIME          2  // (seconds) Time to wait for hotend to "settle" in M109
+#define TEMP_HYSTERESIS              5  // (°C) Temperature proximity considered "close enough" to the target
+#define TEMP_WINDOW                  1  // (°C) Temperature proximity for the "temperature reached" timer
 
 #define TEMP_BED_RESIDENCY_TIME     10  // (seconds) Time to wait for bed to "settle" in M190
 #define TEMP_BED_WINDOW              1  // (°C) Temperature proximity for the "temperature reached" timer
