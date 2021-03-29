21cba74aeda046b38bf2f235f936d622f0c196f1
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2021-03-28 17:31:59 -0700

    Tevo: Assume nominal belts/pulleys (#445)

--- config/examples/Tevo/Tarantula Pro/Configuration.h
+++ config/examples/Tevo/Tarantula Pro/Configuration.h
@@ -799,7 +799,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.058, 80.058, 399.2901, 408 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 408 }
 
 /**
  * Default Max Feed Rate (mm/s)
--- config/examples/Tevo/Tornado/V1 (MKS Base)/Configuration.h
+++ config/examples/Tevo/Tornado/V1 (MKS Base)/Configuration.h
@@ -799,7 +799,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.765, 79.765, 399.2901, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
