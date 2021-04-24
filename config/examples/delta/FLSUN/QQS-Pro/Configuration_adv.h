76a5b8fa608e8318fd7b1f54cb6e7ead56a1ef34
Scott Lahteine <github@thinkyhead.com>
2021-04-24 01:51:03 -0500

    Misc. cleanup

--- config/examples/delta/FLSUN/QQS-Pro/Configuration_adv.h
+++ config/examples/delta/FLSUN/QQS-Pro/Configuration_adv.h
@@ -941,7 +941,7 @@
 
 // Slow down the machine if the lookahead buffer is (by default) half full.
 // Increase the slowdown divisor for larger buffer sizes.
-//(don't use SLOWDOWN with DELTA because DELTA generates hundreds of segments per second)
+// (don't use SLOWDOWN with DELTA because DELTA generates hundreds of segments per second)
 //#define SLOWDOWN
 #if ENABLED(SLOWDOWN)
   #define SLOWDOWN_DIVISOR 2
