eec1d8877fd214eb5c95982951fa2f9397fcb207
Ryan Finnie <ryan@finnie.org>
2021-05-08 20:04:52 -0700

    Fix Creality V4 BABYSTEP_MULTIPLICATOR_Z (#479)

--- config/examples/Creality/Ender-3 V2/Configuration_adv.h
+++ config/examples/Creality/Ender-3 V2/Configuration_adv.h
@@ -1799,7 +1799,7 @@
   //#define BABYSTEP_XY                     // Also enable X/Y Babystepping. Not supported on DELTA!
   #define BABYSTEP_INVERT_Z false           // Change if Z babysteps should go the other way
   //#define BABYSTEP_MILLIMETER_UNITS       // Specify BABYSTEP_MULTIPLICATOR_(XY|Z) in mm instead of micro-steps
-  #define BABYSTEP_MULTIPLICATOR_Z  40      // (steps or mm) Steps or millimeter distance for each Z babystep
+  #define BABYSTEP_MULTIPLICATOR_Z  1       // (steps or mm) Steps or millimeter distance for each Z babystep
   #define BABYSTEP_MULTIPLICATOR_XY 1       // (steps or mm) Steps or millimeter distance for each XY babystep
 
   //#define DOUBLECLICK_FOR_Z_BABYSTEPPING  // Double-click on the Status Screen for Z Babystepping.
