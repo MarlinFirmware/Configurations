45f29272ea9407ce939ab63db3fb36262e3245a5
Scott Lahteine <thinkyhead@users.noreply.github.com>
2021-05-11 06:42:06 -0500

    Remove STRAIN_GAUGE_PROBE

--- config/examples/delta/Tevo Little Monster/Configuration.h
+++ config/examples/delta/Tevo Little Monster/Configuration.h
@@ -1020,12 +1020,6 @@
 //#define FIX_MOUNTED_PROBE
 
 /**
- * Use the nozzle as the probe, with the hotend
- * assembly attached to a sensitive strain gauge.
- */
-//#define STRAIN_GAUGE_PROBE
-
-/**
  * Use the nozzle as the probe, as with a conductive
  * nozzle system or a piezo-electric smart effector.
  */
