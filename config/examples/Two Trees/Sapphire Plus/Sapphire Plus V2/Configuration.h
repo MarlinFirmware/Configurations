9965419146d91a0bc28eb721f28627915356dfc4
Katelyn Schiesser <katelyn.schiesser@gmail.com>
2021-07-11 18:13:48 -0700

    🔧 Fix endstop inverting with BLTouch options (#533)

--- config/examples/Two Trees/Sapphire Plus/Sapphire Plus V2/Configuration.h
+++ config/examples/Two Trees/Sapphire Plus/Sapphire Plus V2/Configuration.h
@@ -824,24 +824,19 @@
 #endif
 
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
-#if DISABLED(SAPPHIRE_PLUS_BLTOUCH)
-  #define SP_INVERTING true
-#else
-  #define SP_INVERTING false
-#endif
 #define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define Y_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_ENDSTOP_INVERTING SP_INVERTING // Set to true to invert the logic of the endstop.
+#define Z_MIN_ENDSTOP_INVERTING DISABLED(SAPPHIRE_PLUS_BLTOUCH) // Set to true to invert the logic of the endstop.
 #define I_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define J_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define K_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define X_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define Y_MAX_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-#define Z_MAX_ENDSTOP_INVERTING SP_INVERTING // Set to true to invert the logic of the endstop.
+#define Z_MAX_ENDSTOP_INVERTING DISABLED(SAPPHIRE_PLUS_BLTOUCH) // Set to true to invert the logic of the endstop.
 #define I_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define J_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define K_MAX_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
-#define Z_MIN_PROBE_ENDSTOP_INVERTING SP_INVERTING // Set to true to invert the logic of the probe.
+#define Z_MIN_PROBE_ENDSTOP_INVERTING false // Set to true to invert the logic of the probe.
 
 /**
  * Stepper Drivers
