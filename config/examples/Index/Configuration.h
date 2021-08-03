22d37ab3991e1a71b16b07a90cd0e1e381355f65
Justin Nesselrotte <admin@jnesselr.org>
2021-08-03 17:18:36 -0600

    Adjusted Z max pos to 59 instead of 60
    29.5 was the center, so might as well make double that be the total
    distance.

--- config/examples/Index/Configuration.h
+++ config/examples/Index/Configuration.h
@@ -1368,7 +1368,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 60
+#define Z_MAX_POS 59
 #define I_MIN_POS 0
 #define I_MAX_POS 50
 #define J_MIN_POS 0
