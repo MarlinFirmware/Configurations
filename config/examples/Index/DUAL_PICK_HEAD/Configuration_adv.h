d6f82ba62585c58f78cb1dabd454df58a81f2231
Justin Nesselrotte <admin@jnesselr.org>
2021-08-27 23:41:27 -0600

    Added RS485 hardware serial port to config

--- config/examples/Index/DUAL_PICK_HEAD/Configuration_adv.h
+++ config/examples/Index/DUAL_PICK_HEAD/Configuration_adv.h
@@ -4164,3 +4164,8 @@
  */
 //#define SOFT_RESET_VIA_SERIAL         // 'KILL' and '^X' commands will soft-reset the controller
 //#define SOFT_RESET_ON_KILL            // Use a digital button to soft-reset the controller after KILL
+
+/**
+ * RS485 Hardware serial port definition
+ */
+#define RS485_SERIAL_PORT           1
