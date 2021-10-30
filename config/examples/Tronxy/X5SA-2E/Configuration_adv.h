c9578dfad1045a22fa79ba0f043c700ce705f462
Scott Lahteine <github@thinkyhead.com>
2021-10-29 19:27:39 -0500

    ✨ HOST_SHUTDOWN_MENU_ITEM
    MarlinFirmware/Marlin#22908

--- config/examples/Tronxy/X5SA-2E/Configuration_adv.h
+++ config/examples/Tronxy/X5SA-2E/Configuration_adv.h
@@ -3849,7 +3849,8 @@
 #define HOST_ACTION_COMMANDS
 #if ENABLED(HOST_ACTION_COMMANDS)
   //#define HOST_PROMPT_SUPPORT
-  //#define HOST_START_MENU_ITEM  // Add a menu item that tells the host to start
+  //#define HOST_START_MENU_ITEM      // Add a menu item that tells the host to start
+  //#define HOST_SHUTDOWN_MENU_ITEM   // Add a menu item that tells the host to shut down
 #endif
 
 /**
