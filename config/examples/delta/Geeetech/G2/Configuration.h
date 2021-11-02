4339d8434ca3d880ad0fec4aedb0ffe969e97ecf
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2021-11-01 22:04:16 -0700

    Restore Low EJERK Configs

--- config/examples/delta/Geeetech/G2/Configuration.h
+++ config/examples/delta/Geeetech/G2/Configuration.h
@@ -1042,7 +1042,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK   10.0  // May be used by Linear Advance
+#define DEFAULT_EJERK   5.0  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
