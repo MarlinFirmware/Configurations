e31b801d195cbd8838532e3c10bd852a9c0ac641
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2021-11-14 14:29:42 -0800

    🚸 B1 SE Plus - Probing Preheat (#614)

--- config/examples/BIQU/B1 SE Plus/Configuration.h
+++ config/examples/BIQU/B1 SE Plus/Configuration.h
@@ -1291,9 +1291,9 @@
 //#define DELAY_BEFORE_PROBING 200  // (ms) To prevent vibrations from triggering piezo sensors
 
 // Require minimum nozzle and/or bed temperature for probing
-//#define PREHEAT_BEFORE_PROBING
+#define PREHEAT_BEFORE_PROBING
 #if ENABLED(PREHEAT_BEFORE_PROBING)
-  #define PROBING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
+  #define PROBING_NOZZLE_TEMP 145   // (°C) Only applies to E0 at this time
   #define PROBING_BED_TEMP     50
 #endif
 
