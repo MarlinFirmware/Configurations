e7d789ec4937feaa17761f43e874fa8b0e7fa0b4
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2021-11-24 03:25:41 -0800

    Remove Bidirectional Unicode Text

--- config/examples/Creality/CR-10 Mini/MEEB-3DP/Configuration_adv.h
+++ config/examples/Creality/CR-10 Mini/MEEB-3DP/Configuration_adv.h
@@ -1667,7 +1667,7 @@
   //#define STATUS_ALT_FAN_BITMAP     // Use the alternative fan bitmap
   #define STATUS_FAN_FRAMES 4
   #define STATUS_HEAT_PERCENT       // Show heating in a progress bar
-  #define BOOT_MARLIN_LOGO_ANIMATED // Animated Marlin logo. Costs ~‭3260 (or ~940) bytes of PROGMEM.
+  #define BOOT_MARLIN_LOGO_ANIMATED // Animated Marlin logo. Costs ~3260 (or ~940) bytes of PROGMEM.
 
   // Frivolous Game Options
   #define MARLIN_BRICKOUT
