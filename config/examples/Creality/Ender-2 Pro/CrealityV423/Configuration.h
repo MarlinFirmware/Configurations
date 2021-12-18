a486c5ecd59db05136aef8f1a02f2bbc06504586
Spencer Owen <owenspencer@gmail.com>
2021-12-18 11:36:48 -0700

    Change end positions

--- config/examples/Creality/Ender-2 Pro/CrealityV423/Configuration.h
+++ config/examples/Creality/Ender-2 Pro/CrealityV423/Configuration.h
@@ -139,7 +139,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_CREALITY_V4
+  #define MOTHERBOARD BOARD_CREALITY_V423
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
@@ -1382,8 +1382,8 @@
 #define Y_BED_SIZE 165
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -18
+#define Y_MIN_POS -2
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
