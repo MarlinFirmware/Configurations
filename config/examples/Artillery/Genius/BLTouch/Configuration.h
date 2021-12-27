4cb2fcf49538586977af1423771fe7f13414bd8c
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2021-12-27 06:10:47 -0800

    Use nominal pulley values for other Artillery builds

--- config/examples/Artillery/Genius/BLTouch/Configuration.h
+++ config/examples/Artillery/Genius/BLTouch/Configuration.h
@@ -927,7 +927,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.121, 80.121, 399.778, 445 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 445 }
 
 /**
  * Default Max Feed Rate (mm/s)
