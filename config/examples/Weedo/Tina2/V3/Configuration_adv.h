5d3fda4b252f46e593421e4205855d9383f4f442
Julien Staub <atsju2@yahoo.fr>
2022-02-27 17:53:51 +0100

    better abort position

--- config/examples/Weedo/Tina2/V3/Configuration_adv.h
+++ config/examples/Weedo/Tina2/V3/Configuration_adv.h
@@ -1451,7 +1451,7 @@
 
   //#define MEDIA_MENU_AT_TOP               // Force the media menu to be listed on the top of the main menu
 
-  #define EVENT_GCODE_SD_ABORT "G28XY"      // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
+  #define EVENT_GCODE_SD_ABORT "G27"        // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
 
   #if ENABLED(PRINTER_EVENT_LEDS)
     #define PE_LEDS_COMPLETED_TIME  (30*60) // (seconds) Time to keep the LED "done" color before restoring normal illumination
