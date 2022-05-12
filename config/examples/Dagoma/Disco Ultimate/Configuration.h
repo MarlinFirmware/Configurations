a931c0e904ad225331bee2dc9833fd641632240b
Orel <37673727+0r31@users.noreply.github.com>
2022-05-12 10:35:07 +0200

    🐛 Fix Dagoma M106 num fans (#728)

--- config/examples/Dagoma/Disco Ultimate/Configuration.h
+++ config/examples/Dagoma/Disco Ultimate/Configuration.h
@@ -2877,7 +2877,7 @@
 
 // Set number of user-controlled fans. Disable to use all board-defined fans.
 // :[1,2,3,4,5,6,7,8]
-//#define NUM_M106_FANS 1
+#define NUM_M106_FANS 1
 
 // Use software PWM to drive the fan, as for the heaters. This uses a very low frequency
 // which is not as annoying as with the hardware PWM. On the other hand, if this frequency
