cace3992dbfc53c4219515fff973cb4fc9c43646
0Hertz <59251860+0Hertz@users.noreply.github.com>
2022-06-06 21:32:29 +0300

    Default Corrections

--- config/examples/Sovol/SV-01/BigTreeTech SKR Mini E3 3.0/Stock/Configuration_adv.h
+++ config/examples/Sovol/SV-01/BigTreeTech SKR Mini E3 3.0/Stock/Configuration_adv.h
@@ -1361,7 +1361,7 @@
  // #define LCD_DECIMAL_SMALL_XY
 
   // Add an 'M73' G-code to set the current percentage
-  #define LCD_SET_PROGRESS_MANUALLY
+  //#define LCD_SET_PROGRESS_MANUALLY
 
   // Show the E position (filament used) during printing
   //#define LCD_SHOW_E_TOTAL
@@ -1397,9 +1397,9 @@
 // LCD Print Progress options
 #if EITHER(SDSUPPORT, LCD_SET_PROGRESS_MANUALLY)
   #if CAN_SHOW_REMAINING_TIME
-    #define SHOW_REMAINING_TIME         // Display estimated time to completion
+    //#define SHOW_REMAINING_TIME         // Display estimated time to completion
     #if ENABLED(SHOW_REMAINING_TIME)
-      #define USE_M73_REMAINING_TIME    // Use remaining time from M73 command instead of estimation
+      //#define USE_M73_REMAINING_TIME    // Use remaining time from M73 command instead of estimation
       //#define ROTATE_PROGRESS_DISPLAY   // Display (P)rogress, (E)lapsed, and (R)emaining time
     #endif
   #endif
