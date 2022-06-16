bc193aabb53cf7082b91a0ddb1267da0c51cbe10
Andrew J. Kroll <xxxajk@gmail.com>
2022-06-16 03:40:20 -0400

    Add errors, however this config is for 2.1-bugfix

--- config/examples/Teensy/35_36_SIMPLE/Mini2_Hibiscus_Experimental/Configuration.h
+++ config/examples/Teensy/35_36_SIMPLE/Mini2_Hibiscus_Experimental/Configuration.h
@@ -20,6 +20,8 @@
  *
  */
 #pragma once
+#error "Don't build with import-2.1.x configurations!"
+#error "Use the 'bugfix...' or 'release...' configurations matching your Marlin 
 
 /**
  * NOTE! This config file is a bare-bones starting point for replacing
