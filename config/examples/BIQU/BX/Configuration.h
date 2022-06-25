19f635eae5e69d96cb993568ae6954e473907c60
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2022-06-24 17:12:09 -0700

    Individual Axis Homing

--- config/examples/BIQU/BX/Configuration.h
+++ config/examples/BIQU/BX/Configuration.h
@@ -2319,8 +2319,8 @@
 //
 // Add individual axis homing items (Home X, Home Y, and Home Z) to the LCD menu.
 //
-//#define INDIVIDUAL_AXIS_HOMING_MENU
-//#define INDIVIDUAL_AXIS_HOMING_SUBMENU
+#define INDIVIDUAL_AXIS_HOMING_MENU
+#define INDIVIDUAL_AXIS_HOMING_SUBMENU
 
 //
 // SPEAKER/BUZZER
