dbc83fab9ee100056d2dcbd28b5043efa8df6e55
EvilGremlin <evilgremlin8@gmail.com>
2022-08-19 03:01:33 +0300

    oops

--- config/examples/Elegoo/Neptune 2/Configuration.h
+++ config/examples/Elegoo/Neptune 2/Configuration.h
@@ -22,8 +22,8 @@
  */
 
 #pragma once
-// #error "Don't build with import-2.1.x configurations!"
-// #error "Use the 'bugfix...' or 'release...' configurations matching your Marlin version."
+#error "Don't build with import-2.1.x configurations!"
+#error "Use the 'bugfix...' or 'release...' configurations matching your Marlin version."
 
 /**
  * Configuration.h
