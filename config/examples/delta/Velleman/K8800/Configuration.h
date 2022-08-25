f3b40006cca8eb3edd75ce36067a83d487419acd
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-08-25 17:53:31 -0500

    🚸 Up to 10 Preheat Constants
    MarlinFirmware/Marlin#24636

--- config/examples/delta/Velleman/K8800/Configuration.h
+++ config/examples/delta/Velleman/K8800/Configuration.h
@@ -2249,7 +2249,7 @@
 // @section temperature
 
 //
-// Preheat Constants - Up to 6 are supported without changes
+// Preheat Constants - Up to 10 are supported without changes
 //
 #define PREHEAT_1_LABEL       "PLA (180)"
 #define PREHEAT_1_TEMP_HOTEND 200
