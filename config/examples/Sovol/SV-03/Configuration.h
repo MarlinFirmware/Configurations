ddba5e21cc2b2e677aa2e32c5a01874d51972123
ils15 <igorleite-ils@hotmail.com>
2022-08-26 12:32:43 -0300

    Optimized SV03 PID

--- config/examples/Sovol/SV-03/Configuration.h
+++ config/examples/Sovol/SV-03/Configuration.h
@@ -664,9 +664,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 29.76
-    #define DEFAULT_Ki 2.93
-    #define DEFAULT_Kd 75.58
+    #define DEFAULT_Kp 31.3
+    #define DEFAULT_Ki 3.44
+    #define DEFAULT_Kd 71.2
   #endif
 #endif
 
@@ -749,9 +749,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 266.5
+  #define DEFAULT_bedKi 44.56
+  #define DEFAULT_bedKd 62.3
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
