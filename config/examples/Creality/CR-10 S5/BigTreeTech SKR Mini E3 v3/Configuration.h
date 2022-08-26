27e44456cc509d079152821d09200171d47f27de
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-08-25 22:22:36 -0500

    📝 Misc. schema updates

--- config/examples/Creality/CR-10 S5/BigTreeTech SKR Mini E3 v3/Configuration.h
+++ config/examples/Creality/CR-10 S5/BigTreeTech SKR Mini E3 v3/Configuration.h
@@ -729,6 +729,7 @@
  * impact FET heating. This also works fine on a Fotek SSR-10DA Solid State Relay into a 250W
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
+ * @section bed temp
  */
 //#define PIDTEMPBED - CR-10 S5 config
 
