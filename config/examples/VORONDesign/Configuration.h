1439d9f0f2f60f5a99d14f5ec07db600ad0c51bd
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2022-09-28 09:42:29 -0700

    VORON V2 => V1

--- config/examples/VORONDesign/Configuration.h
+++ config/examples/VORONDesign/Configuration.h
@@ -137,7 +137,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "VORON 2.0"
+#define CUSTOM_MACHINE_NAME "VORON 1.0"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
