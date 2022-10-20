2ffa372be491dfbc25a1ab4b18e7252990940026
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-10-20 16:45:30 -0500

    date, error-check

--- config/examples/Tronxy/X5S-2E_CXY-V5-180409/Configuration.h
+++ config/examples/Tronxy/X5S-2E_CXY-V5-180409/Configuration.h
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2022 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -20,6 +20,8 @@
  *
  */
 #pragma once
+#error "Don't build with import-2.1.x configurations!"
+#error "Use the 'bugfix...' or 'release...' configurations matching your Marlin version."
 
 /**
  * Configuration.h
