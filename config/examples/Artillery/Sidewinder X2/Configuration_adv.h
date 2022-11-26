1328cb2cb3c08f8dcd09b9271a484517bd5c7b85
Fabien Devaux <fdev31@gmail.com>
2022-11-26 04:17:51 +0100

    🩹 Fix Z2 stepper for Sidewinder X2 (#844)

--- config/examples/Artillery/Sidewinder X2/Configuration_adv.h
+++ config/examples/Artillery/Sidewinder X2/Configuration_adv.h
@@ -964,7 +964,7 @@
  * Z Steppers Auto-Alignment
  * Add the G34 command to align multiple Z steppers using a bed probe.
  */
-//#define Z_STEPPER_AUTO_ALIGN
+#define Z_STEPPER_AUTO_ALIGN
 #if ENABLED(Z_STEPPER_AUTO_ALIGN)
   /**
    * Define probe X and Y positions for Z1, Z2 [, Z3 [, Z4]]
