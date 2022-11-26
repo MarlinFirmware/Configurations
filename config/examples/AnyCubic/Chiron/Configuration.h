e05ad0af102fe99c3a5344c64ac86a0d2c13c158
Scott Lahteine <thinkyhead@users.noreply.github.com>
2022-11-25 21:23:38 -0600

    50mm/s is not unusual for an X carriage

--- config/examples/AnyCubic/Chiron/Configuration.h
+++ config/examples/AnyCubic/Chiron/Configuration.h
@@ -2108,7 +2108,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (35*60), (35*60), (5*60) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (35*60), (5*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
