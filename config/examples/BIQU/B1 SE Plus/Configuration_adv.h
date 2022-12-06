a1c4306c18c6a8b37c35f55044cbbd62aa7cc0b4
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2022-12-05 19:03:37 -0800

    🔧 Fan 2 for Biqu B1 SE Plus (#837)

--- config/examples/BIQU/B1 SE Plus/Configuration_adv.h
+++ config/examples/BIQU/B1 SE Plus/Configuration_adv.h
@@ -535,10 +535,10 @@
  * The fan turns on automatically whenever any driver is enabled and turns
  * off (or reduces to idle speed) shortly after drivers are turned off.
  */
-//#define USE_CONTROLLER_FAN
+#define USE_CONTROLLER_FAN
 #if ENABLED(USE_CONTROLLER_FAN)
-  //#define CONTROLLER_FAN_PIN -1           // Set a custom pin for the controller fan
-  //#define CONTROLLER_FAN2_PIN -1          // Set a custom pin for second controller fan
+  #define CONTROLLER_FAN_PIN       FAN2_PIN // Set a custom pin for the controller fan
+  #define CONTROLLER_FAN2_PIN      FAN1_PIN // Set a custom pin for second controller fan
   //#define CONTROLLER_FAN_USE_Z_ONLY       // With this option only the Z axis is considered
   //#define CONTROLLER_FAN_IGNORE_Z         // Ignore Z stepper. Useful when stepper timeout is disabled.
   #define CONTROLLERFAN_SPEED_MIN         0 // (0-255) Minimum speed. (If set below this value the fan is turned off.)
