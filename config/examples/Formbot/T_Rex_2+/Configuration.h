9658e52f2acd785d7d771c435cf821b0153ce8e1
ellensp <530024+ellensp@users.noreply.github.com>
2022-12-18 16:17:17 +1300

    🩹 Fix Formbot T_Rex_2+ SD Detect (#864)

--- config/examples/Formbot/T_Rex_2+/Configuration.h
+++ config/examples/Formbot/T_Rex_2+/Configuration.h
@@ -1661,7 +1661,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR true
+#define INVERT_E0_DIR true   // Might be 'false' on your machine. Change if your extruder goes backwards.
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
