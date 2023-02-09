b4ecf58834116085a04c487121e99cf387c1ce87
ellensp <530024+ellensp@users.noreply.github.com>
2023-02-09 18:41:44 +1300

    🐛 Fix AnyCubic Mega Zero 2.0 LCD (#884)

--- config/examples/AnyCubic/Mega Zero 2.0/Anycubic V1/Configuration.h
+++ config/examples/AnyCubic/Mega Zero 2.0/Anycubic V1/Configuration.h
@@ -2800,7 +2800,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // https://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+//#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // K.3D Full Graphic Smart Controller
@@ -2907,7 +2907,7 @@
 // This is RAMPS-compatible using a single 10-pin connector.
 // (For CR-10 owners who want to replace the Melzi Creality board but retain the display)
 //
-//#define CR10_STOCKDISPLAY
+#define CR10_STOCKDISPLAY
 
 //
 // Ender-2 OEM display, a variant of the MKS_MINI_12864
