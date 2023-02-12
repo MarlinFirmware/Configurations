6063fd59ff90471f89a3c2ec70949bc4aaffce5c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-02-11 22:44:49 -0600

    🚸 Progress display followup
    MarlinFirmware/Marlin#24879

--- config/examples/Creality/CR-10 V3/Configuration_adv.h
+++ config/examples/Creality/CR-10 V3/Configuration_adv.h
@@ -1495,8 +1495,8 @@
 // Add 'M73' to set print job progress, overrides Marlin's built-in estimate
 #define SET_PROGRESS_MANUALLY
 #if ENABLED(SET_PROGRESS_MANUALLY)
-  #define SET_PROGRESS_PERCENT            // Add 'P' parameter to set percentage done, otherwise use Marlin's estimate
-  //#define SET_REMAINING_TIME            // Add 'R' parameter to set remaining time, otherwise use Marlin's estimate
+  #define SET_PROGRESS_PERCENT            // Add 'P' parameter to set percentage done
+  //#define SET_REMAINING_TIME            // Add 'R' parameter to set remaining time
   //#define SET_INTERACTION_TIME          // Add 'C' parameter to set time until next filament change or other user interaction
   //#define M73_REPORT                    // Report M73 values to host
   #if BOTH(M73_REPORT, SDSUPPORT)
