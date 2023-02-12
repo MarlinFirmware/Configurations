0aedf13086998c95ef43625a0fc1ece84e68b80c
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-02-11 22:38:52 -0600

    🎨 Combine HAS_DISPLAY blocks

--- config/examples/BQ/Hephestos_2/Configuration_adv.h
+++ config/examples/BQ/Hephestos_2/Configuration_adv.h
@@ -1444,9 +1444,8 @@
 #if HAS_DISPLAY
   //#define SOUND_MENU_ITEM   // Add a mute option to the LCD menu
   #define SOUND_ON_DEFAULT    // Buzzer/speaker default enabled state
-#endif
 
-#if HAS_DISPLAY  // The timeout to return to the status screen from sub-menus
+  // The timeout to return to the status screen from sub-menus
   #define LCD_TIMEOUT_TO_STATUS 60000     // (ms)
 
   #if ENABLED(SHOW_BOOTSCREEN)
