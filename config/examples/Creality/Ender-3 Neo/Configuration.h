e6c3e84c19cf9741b6f484b25b472a8547deec1d
tontonlex <124630015+tontonlex@users.noreply.github.com>
2023-02-14 07:21:45 +0100

    🚸 Adjust Creality Ender-3 Neo bed position (#892)

--- config/examples/Creality/Ender-3 Neo/Configuration.h
+++ config/examples/Creality/Ender-3 Neo/Configuration.h
@@ -1761,8 +1761,8 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -27
-#define Y_MIN_POS -11
+#define X_MIN_POS -20
+#define Y_MIN_POS -7
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
