facc0c65e8cf4fc46321c8d204898adf3555a8d3
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2023-03-26 11:20:46 -0700

    Reduce Driver Currents
    Original values ported from Klipper config, but motors don't need to run that hot, especially without enabling Input Shaping.

--- config/examples/BIQU/Hurakan/Configuration_adv.h
+++ config/examples/BIQU/Hurakan/Configuration_adv.h
@@ -2781,7 +2781,7 @@
   #define INTERPOLATE      true
 
   #if AXIS_IS_TMC_CONFIG(X)
-    #define X_CURRENT       1000       // (mA) RMS current. Multiply by 1.414 for peak current.
+    #define X_CURRENT       800        // (mA) RMS current. Multiply by 1.414 for peak current.
     #define X_CURRENT_HOME  X_CURRENT  // (mA) RMS current for sensorless homing
     #define X_MICROSTEPS     16        // 0..256
     #define X_RSENSE          0.11     // Multiplied x1000 for TMC26X
@@ -2801,7 +2801,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Y)
-    #define Y_CURRENT       1000
+    #define Y_CURRENT       800
     #define Y_CURRENT_HOME  Y_CURRENT
     #define Y_MICROSTEPS     16
     #define Y_RSENSE          0.11
@@ -2921,7 +2921,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(E0)
-    #define E0_CURRENT      950
+    #define E0_CURRENT      850
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
