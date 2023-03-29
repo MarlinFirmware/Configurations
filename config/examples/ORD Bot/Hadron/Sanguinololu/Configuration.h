7fa8b7f1244dbadd3d702e9c637529ca845c3139
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-03-28 23:59:18 -0500

    📝 "Sanguinololu" is ORD Bot Hadron

--- config/examples/ORD Bot/Hadron/Sanguinololu/Configuration.h
+++ config/examples/ORD Bot/Hadron/Sanguinololu/Configuration.h
@@ -137,7 +137,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Sanguinololu"
+#define CUSTOM_MACHINE_NAME "ORD Bot Hadron"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
