6d72df3209c758f5e88e7d7679dad71b44692860
Joe <52463691+joepotato04@users.noreply.github.com>
2023-05-28 22:16:09 +0100

    Update Configuration.h
    changed the MAX_TEMP value as it created errors when trying to build the firmware

--- config/examples/JGAurora/A5/Configuration.h
+++ config/examples/JGAurora/A5/Configuration.h
@@ -626,7 +626,7 @@
 // Above this temperature the heater will be switched off.
 // This can protect components from overheating, but NOT from shorts and failures.
 // (Use MINTEMP for thermistor short/failure protection.)
-#define HEATER_0_MAXTEMP 265
+#define HEATER_0_MAXTEMP 260
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
 #define HEATER_3_MAXTEMP 275
