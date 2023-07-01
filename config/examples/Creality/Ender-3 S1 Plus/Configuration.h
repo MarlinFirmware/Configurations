01578a2bc4bfbdd918751e9cfe97ab822725a5fb
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-07-01 01:29:30 -0500

    add a dash

--- config/examples/Creality/Ender-3 S1 Plus/Configuration.h
+++ config/examples/Creality/Ender-3 S1 Plus/Configuration.h
@@ -62,7 +62,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(strnk, Ender 3 S1 Plus)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(strnk, Ender-3 S1 Plus)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
