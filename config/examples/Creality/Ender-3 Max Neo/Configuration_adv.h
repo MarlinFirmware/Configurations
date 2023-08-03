66b8c69b7ed75a087699186672d99736f80672fc
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-08-02 22:50:35 -0500

    ws

--- config/examples/Creality/Ender-3 Max Neo/Configuration_adv.h
+++ config/examples/Creality/Ender-3 Max Neo/Configuration_adv.h
@@ -1479,7 +1479,7 @@
 //#define LCD_BACKLIGHT_TIMEOUT_MINS 1  // (minutes) Timeout before turning off the backlight
 
 #if HAS_BED_PROBE && ANY(HAS_MARLINUI_MENU, HAS_TFT_LVGL_UI)
-  #define PROBE_OFFSET_WIZARD       // Add a Probe Z Offset calibration option to the LCD menu
+  #define PROBE_OFFSET_WIZARD         // Add a Probe Z Offset calibration option to the LCD menu
   #if ENABLED(PROBE_OFFSET_WIZARD)
     /**
      * Enable to init the Probe Z-Offset when starting the Wizard.
