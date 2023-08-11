e464df270d730bdaae0f24ec58b16d2fe8bfa53f
ellensp <530024+ellensp@users.noreply.github.com>
2023-08-11 20:41:38 +1200

    enable BABYSTEP_ZPROBE_OFFSET

--- config/examples/Creality/CR-10 Max/Configuration_adv.h
+++ config/examples/Creality/CR-10 Max/Configuration_adv.h
@@ -2243,7 +2243,7 @@
 
   //#define BABYSTEP_DISPLAY_TOTAL          // Display total babysteps since last G28
 
-  //#define BABYSTEP_ZPROBE_OFFSET          // Combine M851 Z and Babystepping
+  #define BABYSTEP_ZPROBE_OFFSET          // Combine M851 Z and Babystepping
   //#define BABYSTEP_GLOBAL_Z               // Combine M424 Z and Babystepping
 
   #if ANY(BABYSTEP_ZPROBE_OFFSET, BABYSTEP_GLOBAL_Z)
