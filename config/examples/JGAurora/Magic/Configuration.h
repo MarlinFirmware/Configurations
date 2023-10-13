ce94fb577137a956bdedfd4c285768e8ad6af932
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2023-10-13 04:45:27 -0700

    Filament Runout is on Pin 2, not 4

--- config/examples/JGAurora/Magic/Configuration.h
+++ config/examples/JGAurora/Magic/Configuration.h
@@ -1881,6 +1881,7 @@
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define FIL_RUNOUT_ENABLED_DEFAULT true // Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   1          // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
+  #define FIL_RUNOUT_PIN       2
 
   #define FIL_RUNOUT_STATE     HIGH       // Pin state indicating that filament is NOT present.
   #define FIL_RUNOUT_PULLUP               // Use internal pullup for filament runout pins.
