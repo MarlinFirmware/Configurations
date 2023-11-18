e677f20aabb462c3b34c2183c12ec13b62a4eb8f
Scott Lahteine <thinkyhead@users.noreply.github.com>
2023-11-17 19:40:17 -0600

    ♻️ MANUAL_MOVE_DISTANCE_DEG
    MarlinFirmware/Marlin#26438

--- config/examples/delta/Anycubic/Predator/Configuration_adv.h
+++ config/examples/delta/Anycubic/Predator/Configuration_adv.h
@@ -1535,10 +1535,14 @@
   #define MANUAL_MOVE_DISTANCE_MM                    10, 1.0, 0.1  // (mm)
   //#define MANUAL_MOVE_DISTANCE_MM         100, 50, 10, 1.0, 0.1  // (mm)
   //#define MANUAL_MOVE_DISTANCE_MM    500, 100, 50, 10, 1.0, 0.1  // (mm)
+
   // Manual move distances for INCH_MODE_SUPPORT
   #define MANUAL_MOVE_DISTANCE_IN                          0.100, 0.010, 0.001  // (in)
   //#define MANUAL_MOVE_DISTANCE_IN          1.000, 0.500, 0.100, 0.010, 0.001  // (in)
   //#define MANUAL_MOVE_DISTANCE_IN   5.000, 1.000, 0.500, 0.100, 0.010, 0.001  // (in)
+
+  // Manual move distances for rotational axes
+  #define MANUAL_MOVE_DISTANCE_DEG             90, 45, 22.5, 5, 1  // (°)
 
   // BACK menu items keep the highlight at the top
   //#define TURBO_BACK_MENU_ITEM
