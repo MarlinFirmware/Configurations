5557b644de0d453b99530c68ca53cb27c0548a76
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-02-01 19:06:23 -0600

    clean up

--- config/examples/Turnigy/Fabrikator/Configuration_adv.h
+++ config/examples/Turnigy/Fabrikator/Configuration_adv.h
@@ -1497,7 +1497,7 @@
 #endif
 
 #if HAS_BED_PROBE && ANY(HAS_MARLINUI_MENU, HAS_TFT_LVGL_UI)
-  #define PROBE_OFFSET_WIZARD       // Add a Probe Z Offset calibration option to the LCD menu
+  #define PROBE_OFFSET_WIZARD         // Add a Probe Z Offset calibration option to the LCD menu
   #if ENABLED(PROBE_OFFSET_WIZARD)
     /**
      * Enable to init the Probe Z-Offset when starting the Wizard.
