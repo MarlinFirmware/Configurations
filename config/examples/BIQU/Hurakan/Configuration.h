7b12069eeac5e2660a7362ea35a35e776b78c2ee
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2024-02-08 21:58:03 -0800

    Update NeoPixel Config
    2 NeoPixels for hotend lighting

--- config/examples/BIQU/Hurakan/Configuration.h
+++ config/examples/BIQU/Hurakan/Configuration.h
@@ -3589,14 +3589,14 @@
   #define NEOPIXEL2_TYPE          NEO_GRB
   #define NEOPIXEL2_PIN               PD0
   #define NEOPIXEL_PIXELS               3 // Number of LEDs in the strip. (Longest strip when NEOPIXEL2_SEPARATE is disabled.)
-  #define NEOPIXEL_IS_SEQUENTIAL          // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
+  //#define NEOPIXEL_IS_SEQUENTIAL        // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
   #define NEOPIXEL_BRIGHTNESS         255 // Initial brightness (0-255)
   #define NEOPIXEL_STARTUP_TEST           // Cycle through colors at startup
 
   // Support for second Adafruit NeoPixel LED driver controlled with M150 S1 ...
   #define NEOPIXEL2_SEPARATE
   #if ENABLED(NEOPIXEL2_SEPARATE)
-    #define NEOPIXEL2_PIXELS           10 // Number of LEDs in the second strip
+    #define NEOPIXEL2_PIXELS            2 // Number of LEDs in the second strip
     #define NEOPIXEL2_BRIGHTNESS      255 // Initial brightness (0-255)
     #define NEOPIXEL2_STARTUP_TEST        // Cycle through colors at startup
     #define NEOPIXEL_M150_DEFAULT      -1 // Default strip for M150 without 'S'. Use -1 to set all by default.
