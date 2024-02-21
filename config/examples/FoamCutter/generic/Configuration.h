d84adf379d9eeab8d1860ddb58deb87de4308797
DerAndere <26200979+DerAndere1@users.noreply.github.com>
2024-02-21 22:21:03 +0100

    no Z homing for foamcutters to fix compilation

--- config/examples/FoamCutter/generic/Configuration.h
+++ config/examples/FoamCutter/generic/Configuration.h
@@ -1803,7 +1803,7 @@
 // :[-1,1]
 #define X_HOME_DIR -1
 #define Y_HOME_DIR -1
-#define Z_HOME_DIR -1
+#define Z_HOME_DIR  0
 #define I_HOME_DIR -1
 #define J_HOME_DIR -1
 //#define K_HOME_DIR -1
@@ -2270,7 +2270,7 @@
 // For DELTA this is the top-center of the Cartesian print volume.
 //#define MANUAL_X_HOME_POS 0
 //#define MANUAL_Y_HOME_POS 0
-//#define MANUAL_Z_HOME_POS 0
+#define MANUAL_Z_HOME_POS 0
 //#define MANUAL_I_HOME_POS 0
 //#define MANUAL_J_HOME_POS 0
 //#define MANUAL_K_HOME_POS 0
