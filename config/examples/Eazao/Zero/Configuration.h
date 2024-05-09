16bcd43350c5958cab928c2b54f42d6bf9239edc
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2024-05-09 02:11:11 -0700

    📝 Add Eazao Zero dummy thermistor note

--- config/examples/Eazao/Zero/Configuration.h
+++ config/examples/Eazao/Zero/Configuration.h
@@ -564,7 +564,7 @@
  *   998 : Dummy Table that ALWAYS reads 25°C or the temperature defined below.
  *   999 : Dummy Table that ALWAYS reads 100°C or the temperature defined below.
  */
-#define TEMP_SENSOR_0 998
+#define TEMP_SENSOR_0 998 // Required for TFT firmware to function properly
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_3 0
