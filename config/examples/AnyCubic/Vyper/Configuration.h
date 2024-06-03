0e2695259340724af5898c34aad79a91ee8d64cf
Nicolas Graziano <nicolas.graziano@gmail.com>
2024-06-04 00:45:49 +0200

    AnyCubic Vyper by default disable fade
    The probing is more than 2mm below home Z position
    so fading can not be use without an offset.

--- config/examples/AnyCubic/Vyper/Configuration.h
+++ config/examples/AnyCubic/Vyper/Configuration.h
@@ -2105,7 +2105,7 @@
    */
   #define ENABLE_LEVELING_FADE_HEIGHT
   #if ENABLED(ENABLE_LEVELING_FADE_HEIGHT)
-    #define DEFAULT_LEVELING_FADE_HEIGHT 10.0 // (mm) Default fade height.
+    #define DEFAULT_LEVELING_FADE_HEIGHT 0.0 // (mm) Default fade height.
   #endif
 
   /**
