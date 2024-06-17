73f9e645d34c2614a926201c4fc1eabc7d529ecf
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2024-06-17 11:16:06 -0700

    Fix EVENT_GCODE_SD_ABORT comment

--- config/examples/BIQU/B1 SE Plus/Configuration_adv.h
+++ config/examples/BIQU/B1 SE Plus/Configuration_adv.h
@@ -1558,7 +1558,7 @@
 
   //#define MEDIA_MENU_AT_TOP               // Force the media menu to be listed on the top of the main menu
 
-  #define EVENT_GCODE_SD_ABORT "G28XY\nM84" // G-code to run on Stop Print (e.g., "G28XY" or "G27")
+  #define EVENT_GCODE_SD_ABORT "G28XY\nM84" // G-code to run on SD Abort Print (e.g., "G28XY" or "G27")
 
   #if ENABLED(PRINTER_EVENT_LEDS)
     #define PE_LEDS_COMPLETED_TIME  (30*60) // (seconds) Time to keep the LED "done" color before restoring normal illumination
