8c280ea8748f0df640113bbaeb922cc897d0cc0b
Keith Bennett <13375512+thisiskeithb@users.noreply.github.com>
2024-07-29 11:03:09 -0700

    🎨 Update authors (#1070)
    Co-authored-by: Scott Lahteine <thinkyhead@users.noreply.github.com>
    Co-authored-by: ellensp <530024+ellensp@users.noreply.github.com>

--- config/examples/Geeetech/A10D/Configuration.h
+++ config/examples/Geeetech/A10D/Configuration.h
@@ -63,7 +63,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(default)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Geeetech)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
