1ee567e532fc8e2eb0cd0c938aec2d2e7befefa7
Alvaro Segura Del Barco <alvarosb@gmail.com>
2024-08-03 20:39:51 -0600

    set bed size

--- config/examples/Printrbot/Play_BTT_SKR_Mini_E3_30/Configuration.h
+++ config/examples/Printrbot/Play_BTT_SKR_Mini_E3_30/Configuration.h
@@ -1838,8 +1838,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 100
+#define Y_BED_SIZE 100
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1847,7 +1847,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 130
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
