31eac0f18cc6b85a3dd69af235926d1d574170e2
Alvaro Segura Del Barco <alvarosb@gmail.com>
2024-08-03 20:31:42 -0600

    set DEFAULT_AXIS_STEPS_PER_UNIT same as printrbot

--- config/examples/Printrbot/Play_BTT_SKR_Mini_E3_30/Configuration.h
+++ config/examples/Printrbot/Play_BTT_SKR_Mini_E3_30/Configuration.h
@@ -1247,7 +1247,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2020, 94.5 } // if acme rod change Z to 400
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
