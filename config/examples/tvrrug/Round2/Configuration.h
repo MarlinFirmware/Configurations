ad890a3b53411352d0aa64fcfa0eeac27c5b11eb
Scott Lahteine <thinkyhead@users.noreply.github.com>
2024-08-19 14:55:17 -0500

    📝 BIQU_MICROPROBE_V1 + FT_MOTION
    MarlinFirmware/Marlin#27302

--- config/examples/tvrrug/Round2/Configuration.h
+++ config/examples/tvrrug/Round2/Configuration.h
@@ -1484,7 +1484,8 @@
  * A lightweight, solenoid-driven probe.
  * For information about this sensor https://github.com/bigtreetech/MicroProbe
  *
- * Also requires: PROBE_ENABLE_DISABLE
+ * Also requires PROBE_ENABLE_DISABLE
+ * With FT_MOTION requires ENDSTOP_INTERRUPTS_FEATURE
  */
 //#define BIQU_MICROPROBE_V1  // Triggers HIGH
 //#define BIQU_MICROPROBE_V2  // Triggers LOW
