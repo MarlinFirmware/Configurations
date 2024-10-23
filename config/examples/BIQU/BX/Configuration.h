08f0f19eb5f7c783ceebe6115a22594e2f75a244
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2024-10-22 21:15:43 -0700

    🐛 Fix BIQU BX Z Homing
    A stock BIQU BX homes with a Z limit switch, not the probe. See the README for details.

--- config/examples/BIQU/BX/Configuration.h
+++ config/examples/BIQU/BX/Configuration.h
@@ -1423,7 +1423,7 @@
 //#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
-#define USE_PROBE_FOR_Z_HOMING
+//#define USE_PROBE_FOR_Z_HOMING
 
 /**
  * Z_MIN_PROBE_PIN
@@ -2358,7 +2358,7 @@
  * - Allows Z homing only when XY positions are known and trusted.
  * - If stepper drivers sleep, XY homing may be required again before Z homing.
  */
-#define Z_SAFE_HOMING
+//#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT X_CENTER  // (mm) X point for Z homing
