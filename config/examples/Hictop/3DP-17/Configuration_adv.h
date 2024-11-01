f27eab104536ea9eca5411f7aa6346e5bd48afc9
thisiskeithb <13375512+thisiskeithb@users.noreply.github.com>
2024-11-01 08:29:20 -0700

    💡 Update "default minimum feedrates" comments
    https://github.com/MarlinFirmware/Marlin/pull/27494

--- config/examples/Hictop/3DP-17/Configuration_adv.h
+++ config/examples/Hictop/3DP-17/Configuration_adv.h
@@ -1260,8 +1260,8 @@
 #define DISABLE_IDLE_E    // Shut down all idle extruders
 
 // Default Minimum Feedrates for printing and travel moves
-#define DEFAULT_MINIMUMFEEDRATE       0.0     // (mm/s. °/s for rotational-only moves) Minimum feedrate. Set with M205 S.
-#define DEFAULT_MINTRAVELFEEDRATE     0.0     // (mm/s. °/s for rotational-only moves) Minimum travel feedrate. Set with M205 T.
+#define DEFAULT_MINIMUMFEEDRATE       0.0     // (mm/s) Minimum feedrate. Set with M205 S.
+#define DEFAULT_MINTRAVELFEEDRATE     0.0     // (mm/s) Minimum travel feedrate. Set with M205 T.
 
 // Minimum time that a segment needs to take as the buffer gets emptied
 #define DEFAULT_MINSEGMENTTIME        20000   // (µs) Set with M205 B.
