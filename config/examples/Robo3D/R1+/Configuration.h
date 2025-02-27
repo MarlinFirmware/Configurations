7cc2efac6c1a5b56e295685f23ff5e2b9e9274be
ZafotheNinja <cs3719192@gmail.com>
2025-02-26 20:45:23 -0500

    Update Configuration.h
    Z Steps was configured to over three times higher than actual.

--- config/examples/Robo3D/R1+/Configuration.h
+++ config/examples/Robo3D/R1+/Configuration.h
@@ -1294,7 +1294,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 2560, 723.38 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 723.38 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
