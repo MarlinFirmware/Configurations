f22bc43831fab1e708eed6fbb378fa15baf5d1d3
william-aqn <dark.crimson.d.c.r.m@gmail.com>
2025-07-13 13:10:10 +0300

    Update Configuration.h

--- config/examples/delta/Micromake-D1/MKS-Gen-L-V2.1/Configuration.h
+++ config/examples/delta/Micromake-D1/MKS-Gen-L-V2.1/Configuration.h
@@ -1678,7 +1678,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -22.6 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, 0, -22.57 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
