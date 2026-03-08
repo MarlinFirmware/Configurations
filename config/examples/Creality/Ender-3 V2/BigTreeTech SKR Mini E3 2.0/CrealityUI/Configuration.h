d9fa98d79ea2ddc77d90473dabc411774354b7be
Scott Lahteine <thinkyhead@users.noreply.github.com>
2026-03-08 18:15:01 -0500

    ✨ REVERSE_ENCODER_MENU_ITEM
    MarlinFirmware/Marlin#26963
    
    Co-Authored-By: Andrew <18502096+classicrocker883@users.noreply.github.com>

--- config/examples/Creality/Ender-3 V2/BigTreeTech SKR Mini E3 2.0/CrealityUI/Configuration.h
+++ config/examples/Creality/Ender-3 V2/BigTreeTech SKR Mini E3 2.0/CrealityUI/Configuration.h
@@ -3604,6 +3604,10 @@
 #endif
 #define NO_CONTROLLER_CUSTOM_WIRING_WARNING
 
+#if ENABLED(DWIN_LCD_PROUI)
+  #define REVERSE_ENCODER_MENU_ITEM   // Enable menu item to toggle direction
+#endif
+
 //
 // Touch Screen Settings
 //
