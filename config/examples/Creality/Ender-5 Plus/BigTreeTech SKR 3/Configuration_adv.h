d57fc938cc48694a9d6f3c53765d0484ca7ef02f
Scott Lahteine <thinkyhead@users.noreply.github.com>
2026-05-20 12:51:57 -0500

    Maximize probed area

--- config/examples/Creality/Ender-5 Plus/BigTreeTech SKR 3/Configuration_adv.h
+++ config/examples/Creality/Ender-5 Plus/BigTreeTech SKR 3/Configuration_adv.h
@@ -2538,8 +2538,8 @@
   // Override the mesh area if the automatic (max) area is too large
   #define MESH_MIN_X MESH_INSET
   #define MESH_MIN_Y MESH_INSET
-  #define MESH_MAX_X X_BED_SIZE - (MESH_INSET)
-  #define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
+  #define MESH_MAX_X (X_BED_SIZE - 44 - (MESH_INSET))
+  #define MESH_MAX_Y (Y_BED_SIZE -  5 - (MESH_INSET))
 #endif
 
 #if ALL(AUTO_BED_LEVELING_UBL, EEPROM_SETTINGS)
--- config/examples/Creality/Ender-5 Plus/CrealityV1 (ORIGIN UI)/Configuration.h
+++ config/examples/Creality/Ender-5 Plus/CrealityV1 (ORIGIN UI)/Configuration.h
@@ -2306,7 +2306,7 @@
 
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 45             // Set Mesh bounds as an inset region of the bed
+  #define MESH_INSET  1             // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X  7      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
