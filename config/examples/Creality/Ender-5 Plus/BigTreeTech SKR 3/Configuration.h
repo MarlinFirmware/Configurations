e4b25f6c6b69a49bff22828bc77a80b00ef0c425
Steven Haigh <netwiz@crc.id.au>
2026-06-23 14:51:58 +1000

    Fix other example configs here with the same issue too

--- config/examples/Creality/Ender-5 Plus/BigTreeTech SKR 3/Configuration.h
+++ config/examples/Creality/Ender-5 Plus/BigTreeTech SKR 3/Configuration.h
@@ -1373,7 +1373,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 6250, 6250, 15, 15000 }
+#define DEFAULT_MAX_FEEDRATE          { 6250, 6250, 15, 5000 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
