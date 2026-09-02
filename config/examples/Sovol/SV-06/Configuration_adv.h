d4b68b4e77873d903cd006104306895e32b29259
Scott Lahteine <thinkyhead@users.noreply.github.com>
2026-09-01 19:46:26 -0500

    back-off also recommended

--- config/examples/Sovol/SV-06/Configuration_adv.h
+++ config/examples/Sovol/SV-06/Configuration_adv.h
@@ -965,7 +965,7 @@
 #define HOMING_BUMP_MM      { 0, 0, 2 }       // (linear=mm, rotational=°) Backoff from endstops after first bump
 #define HOMING_BUMP_DIVISOR { 2, 2, 4 }       // Re-Bump Speed Divisor (Divides the Homing Feedrate)
 
-//#define HOMING_BACKOFF_POST_MM { 2, 2, 2 }  // (linear=mm, rotational=°) Backoff from endstops after homing
+#define HOMING_BACKOFF_POST_MM { 2, 2, 0 }    // (linear=mm, rotational=°) Backoff from endstops after homing
 //#define XY_COUNTERPART_BACKOFF_MM 0         // (mm) Backoff X after homing Y, and vice-versa
 
 //#define QUICK_HOME                          // If G28 contains XY do a diagonal move first
